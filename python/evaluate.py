44e52bc82d19eb2c2fd465fc73abae1307ec82ba
momohatt <momoko.hattori.1006@gmail.com>
2017-07-13 20:44:13 +0900

    どうするのがいいのかねえ...

--- python/evaluate.py
+++ python/evaluate.py
@@ -5,7 +5,7 @@ import json
 import logging
 import random
 
-w = [770.3, 4.2, -21.8, 64.0, -221.6, -4.0, 20.4, 22.0]
+w = [818.9, -25.2, 11.8, 6.3, 87.8, -237.9, -40.0, -49.4, -559.0, -1063.2]
 
 def Evaluate(g, myself): ##player: represents for whom this board is worth this point
     tmp_board = copy.deepcopy(g._board["Pieces"])
@@ -21,21 +21,30 @@ def Evaluate(g, myself): ##player: represents for whom this board is worth this 
     #        score += piece * w[0]
 
     score += (tmp_board[0][0] + tmp_board[0][7] + tmp_board[7][0] + tmp_board[7][7]) * w[0]
+
     score += (tmp_board[3][3] + tmp_board[3][4] + tmp_board[4][3] + tmp_board[4][4]) * w[1]
+    
     score += (tmp_board[0][1] + tmp_board[0][6] + tmp_board[1][0] + tmp_board[1][7] 
             + tmp_board[6][0] + tmp_board[6][7] + tmp_board[7][1] + tmp_board[7][6]) * w[2]
-    score += (tmp_board[0][2] + tmp_board[0][3] + tmp_board[0][4] + tmp_board[0][5]
-            + tmp_board[2][0] + tmp_board[2][7] + tmp_board[3][0] + tmp_board[3][7]
-            + tmp_board[4][0] + tmp_board[4][7] + tmp_board[5][0] + tmp_board[5][7] 
-            + tmp_board[7][2] + tmp_board[7][3] + tmp_board[7][4] + tmp_board[7][5]) * w[3]
-    score += (tmp_board[1][1] + tmp_board[1][6] + tmp_board[6][1] + tmp_board[6][6]) * w[4]
-    score += (tmp_board[1][2] + tmp_board[1][3] + tmp_board[1][4] + tmp_board[1][5] 
-            + tmp_board[2][1] + tmp_board[2][6] + tmp_board[3][1] + tmp_board[3][6]
-            + tmp_board[4][1] + tmp_board[4][6] + tmp_board[5][1] + tmp_board[5][6] 
-            + tmp_board[6][2] + tmp_board[6][3] + tmp_board[6][4] + tmp_board[6][5]) * w[5]
-    score += (tmp_board[2][2] + tmp_board[2][5] + tmp_board[5][2] + tmp_board[5][5]) * w[6]
+    
+    score += (tmp_board[0][2] + tmp_board[0][5] + tmp_board[2][0] + tmp_board[2][7] 
+            + tmp_board[5][0] + tmp_board[5][7] + tmp_board[7][2] + tmp_board[7][5]) * w[3]
+    
+    score += (tmp_board[0][3] + tmp_board[0][4] + tmp_board[3][0] + tmp_board[3][7]
+            + tmp_board[4][0] + tmp_board[4][7] + tmp_board[7][3] + tmp_board[7][4]) * w[4]
+    
+    score += (tmp_board[1][1] + tmp_board[1][6] + tmp_board[6][1] + tmp_board[6][6]) * w[5]
+    
+    score += (tmp_board[1][2] + tmp_board[1][5] + tmp_board[2][1] + tmp_board[2][6]
+            + tmp_board[5][1] + tmp_board[5][6] + tmp_board[6][2] + tmp_board[6][5]) * w[6]
+    
+    score += (tmp_board[1][3] + tmp_board[1][4] + tmp_board[3][1] + tmp_board[3][6]
+            + tmp_board[4][1] + tmp_board[4][6] + tmp_board[6][3] + tmp_board[6][4]) * w[7]
+    
+    score += (tmp_board[2][2] + tmp_board[2][5] + tmp_board[5][2] + tmp_board[5][5]) * w[8]
+    
     score += (tmp_board[2][3] + tmp_board[2][4] + tmp_board[3][2] + tmp_board[3][5] 
-            + tmp_board[4][2] + tmp_board[4][5] + tmp_board[5][3] + tmp_board[5][4]) * w[7]
+            + tmp_board[4][2] + tmp_board[4][5] + tmp_board[5][3] + tmp_board[5][4]) * w[9]
 
     numOfPieces = 0 
     for row in g._board["Pieces"]:
@@ -43,17 +52,13 @@ def Evaluate(g, myself): ##player: represents for whom this board is worth this 
             if piece != 0:
                 numOfPieces += 1
 
-    if myself == 1:
-        if numOfPieces > 50:
-            return score
-        else:
-            return score * 0.2 + numOfPieces * 80
+    if myself == 2:
+        score *= -1
+    if numOfPieces > 50:
+        return score
     else:
-        if numOfPieces > 50:
-            return -0.2 * score + numOfPieces * 80
-        else:
-            return -1 * score
-
+        print "score = ", score, " numOfPieces * 400 = ", numOfPieces * 400
+        return score + numOfPieces * 400
 
 def MiniMax(g, depth, myself): ##myself: represents for whom we are to forsee the future
     if depth == 0:
@@ -62,38 +67,38 @@ def MiniMax(g, depth, myself): ##myself: represents for whom we are to forsee th
     if len(valid_moves) == 0:
         return Evaluate(g, myself)
 
-    print "next : ", g._board["Next"]
-    print valid_moves
+    #print "next : ", g._board["Next"]
+    #print valid_moves
     #print PrettyPrint(g._board["Pieces"])
     origin_g = copy.deepcopy(g)
     if g._board["Next"] == myself:
-        print "my turn"
+        #print "my turn"
         maximum = -999999
         for move in valid_moves:
             gnext = g.NextBoardPosition(move)
             #print PrettyPrint(gnext._board["Pieces"])
-            print "next : ", gnext._board["Next"]
+            #print "next : ", gnext._board["Next"]
             value = MiniMax(gnext, depth - 1, myself)
             if (value > maximum):
                 maximum = value
-            print "depth : ", depth, " maximum : ", maximum," when ", move
+            #print "depth : ", depth, " maximum : ", maximum," when ", move
             g = copy.deepcopy(origin_g)
-        print "depth : ", depth , " true maximum : ", maximum
+        #print "depth : ", depth , " true maximum : ", maximum
         return maximum
 
     if g._board["Next"] != myself:
-        print "opponent's turn"
+        #print "opponent's turn"
         minimum = 999999
         for move in valid_moves:
             gnext = g.NextBoardPosition(move)
             #print PrettyPrint(gnext._board["Pieces"])
-            print "next : " , gnext._board["Next"]
+            #print "next : " , gnext._board["Next"]
             value = MiniMax(gnext, depth - 1, myself)
             if (value < minimum):
                 minimum = value
-            print "depth : ", depth, " minimum : ", minimum, " when ", move
+            #print "depth : ", depth, " minimum : ", minimum, " when ", move
             g = copy.deepcopy(origin_g)
-        print "depth: ", depth, " true minimum : ", minimum
+        #print "depth: ", depth, " true minimum : ", minimum
         return minimum
 
 
